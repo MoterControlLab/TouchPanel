7be7598557b766afb0ec220e0476043c1a19c31d
foolishfool <foolishfool0418@gmail.com>
2021-08-19 13:59:07 +1000

    [Version] Initial 3D Model and VRTK import

---
+++ Assets/Scripts/Operation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string OpName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Operate()
+    {
+        if (OpName == TouchPanel.Instance.NoticeText.text)
+        {
+            OperateRight();
+        }
+        else
+        {
+            OperateWrong();
+        }
+    }
+    public void OperateRight()
+    {
+        TouchPanel.Instance.GenerateNewCommond();
+    }
+
+
+    public void OperateWrong()
+    {
+        TouchPanel.Instance.GenerateNewCommond();
+    }
+}
---
+++ Assets/Scripts/TouchPanel.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TouchPanel : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI NoticeText;
+
+    public int TestNum;
+
+    static TouchPanel instance;
+    public static TouchPanel Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType(typeof(TouchPanel)) as TouchPanel;
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("TouchPanel");
+                    instance = obj.AddComponent<TouchPanel>();
+                    DontDestroyOnLoad(obj);
+                }
+            }
+            return instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartTestButtonClick()
+    {
+        StartCoroutine(BeginTesting());
+    }
+
+    private IEnumerator BeginTesting()
+    {
+        NoticeText.text = "3";
+        yield return new WaitForSeconds(1);
+        NoticeText.text = "2";
+        yield return new WaitForSeconds(1);
+        NoticeText.text = "1";
+        yield return new WaitForSeconds(1);
+        GenerateNewCommond();
+    }
+
+    public void GenerateNewCommond()
+    {
+        TestNum--;
+        int randomindex = Random.Range(0, 5);
+        switch (randomindex)
+        {
+            case 0:
+                NoticeText.text = "L1";
+                break;
+            case 1:
+                NoticeText.text = "L2";
+                break;
+            case 2:
+                NoticeText.text = "L3";
+                break;
+            case 3:
+                NoticeText.text = "R1";
+                break;
+            case 4:
+                NoticeText.text = "R2";
+                break;
+            case 5:
+                NoticeText.text = "R3";
+                break;
+
+            default:
+                break;
+        }
+
+
+    }
+
+
+}
